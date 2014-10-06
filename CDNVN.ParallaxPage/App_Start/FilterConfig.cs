fa4427b512251b0f55615909fb73bee2dcf9766d
vietpham <kenboy2304@gmail.com>
2014-10-06 10:09:37 +0700

    Okie

---
+++ CDNVN.ParallaxPage/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CDNVN.ParallaxPage
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CDNVN.ParallaxPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDNVN.ParallaxPage.Startup))]
+namespace CDNVN.ParallaxPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
